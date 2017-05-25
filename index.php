fa58b9f5ac1e8a74753683e7f0f4d7991500074a
Pedro Cumino <pedro.cumino@gmail.com>
2017-05-25 11:34:37 -0300

    Adding index.php

--- index.php
+++ index.php
@@ -13,8 +13,12 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+	<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 </head>
 <body>
-	<h1 class="text-center well" >GERCOM2 - UFPA</h1>
+	<h1 class="text-center well"><b>GERCOM2 - UFPA</b></h1>
 </body>
 </html>
